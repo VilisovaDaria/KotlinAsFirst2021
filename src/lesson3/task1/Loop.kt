020ef41ff08de2f4b2f919b5d179449c0fb088a2
VilisovaDaria <daria.v2906@gmail.com>
2021-10-10 19:00:43 +0300

    minD

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -112,7 +112,7 @@ fun fib(n: Int): Int {
  */
 fun minDivisor(n: Int): Int {
     var divisor = 1
-    while (divisor < sqrt(n.toDouble()).toInt()) {
+    while (divisor < n) {
         divisor += 1
         if (n % divisor == 0) return divisor
     }
