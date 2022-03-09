f887b8584ed8be54508a5463e83a1153ce537a60
VilisovaDaria <daria.v2906@gmail.com>
2022-03-09 22:33:01 +0300

    Complex.kt

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -2,10 +2,20 @@
 
 package lesson11.task1
 
+import kotlin.math.pow
+
 /**
  * Фабричный метод для создания комплексного числа из строки вида x+yi
  */
-fun Complex(s: String): Complex = TODO()
+fun Complex(s: String): Complex {
+    try {
+        val plusOrMinus = s.indexOfFirst { it == '+' || it == '-' }
+        return Complex(s.substring(0, plusOrMinus).toDouble(), s.substring(plusOrMinus, s.length - 1).toDouble())
+
+    } catch (e: Exception) {
+        throw IllegalArgumentException()
+    }
+}
 
 /**
  * Класс "комплексное число".
@@ -21,32 +31,41 @@ class Complex(val re: Double, val im: Double) {
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x, 0.0)
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex): Complex = Complex(re + other.re, im + other.im)
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus(): Complex = Complex(-re, -im)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex = Complex(re - other.re, im - other.im)
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex = Complex(
+        re * other.re - im * other.im,
+        re * other.im + im * other.re
+    )
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+    operator fun div(other: Complex): Complex {
+        val divisor = other.re.pow(2) + other.im.pow(2)
+        return Complex(
+            (re * other.re + im * other.im) / divisor,
+            (im * other.re - re * other.im) / divisor
+        )
+    }
 
     /**
      * Сравнение на равенство
